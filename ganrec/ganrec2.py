532cd0e7bdfeffee55eab3b13701f6315079e4d8
XYangXRay <xiaogang.yang@outlook.com>
2023-08-17 14:12:57 -0400

    add propagators.py and config.json

--- ganrec/ganrec2.py
+++ ganrec/ganrec2.py
@@ -1,11 +1,22 @@
+import os
+import numpy as np
+import json
 import tensorflow as tf
 import tensorflow_addons as tfa
 
-import numpy as np
-
+from ganrec.propagators import TomoRadon, PhaseFresnel, PhaseFraunhofer
 from ganrec.models import make_generator, make_discriminator, make_filter
 from ganrec.utils import RECONmonitor, ffactor
 
+
+# Load the configuration from the JSON file
+def load_config(json_file_path):
+    with open(json_file_path, 'r') as file:
+        config = json.load(file)
+    return config
+
+# Use the configuration
+config = load_config('config.json')
 
 # @tf.function
 def discriminator_loss(real_output, fake_output):
@@ -41,12 +52,7 @@ def filer_loss(fake_output, img_output, img_filter):
     return f_loss
 
 
-def tfnor_tomo(img):
-    img = tf.image.per_image_standardization(img)
-    img = img / tf.reduce_max(img)
-    img = img - tf.reduce_min(img)
-    # img = (img - tf.reduce_min(img)) / (tf.reduce_max(img) - tf.reduce_min(img))
-    return img
+
 
 
 def tfnor_phase(img):
@@ -82,63 +88,63 @@ def tomo_bp(sinoi, ang):
     return bp
 
 
-@tf.function
-def tomo_radon(rec, ang):
-    nang = ang.shape[0]
-    img = tf.transpose(rec, [3, 1, 2, 0])
-    img = tf.tile(img, [nang, 1, 1, 1])
-    img = tfa.image.rotate(img, -ang, interpolation='bilinear')
-    sino = tf.reduce_mean(img, 1, name=None)
-    sino = tf.image.per_image_standardization(sino)
-    sino = tf.transpose(sino, [2, 0, 1])
-    sino = tf.reshape(sino, [sino.shape[0], sino.shape[1], sino.shape[2], 1])
-    return sino
+# @tf.function
+# def tomo_radon(rec, ang):
+#     nang = ang.shape[0]
+#     img = tf.transpose(rec, [3, 1, 2, 0])
+#     img = tf.tile(img, [nang, 1, 1, 1])
+#     img = tfa.image.rotate(img, -ang, interpolation='bilinear')
+#     sino = tf.reduce_mean(img, 1, name=None)
+#     sino = tf.image.per_image_standardization(sino)
+#     sino = tf.transpose(sino, [2, 0, 1])
+#     sino = tf.reshape(sino, [sino.shape[0], sino.shape[1], sino.shape[2], 1])
+#     return sino
 
 
-def phase_fresnel(phase, absorption, ff, px):
-    paddings = tf.constant([[px // 2, px // 2], [px // 2, px // 2]])
-    # padding1 = tf.constant([[px // 2, px // 2], [0, 0]])
-    # padding2 = tf.constant([[0, 0], [px // 2, px // 2]])
-    pvalue = tf.reduce_mean(phase[:100, :])
-    # phase = tf.pad(phase, paddings, 'CONSTANT',constant_values=1)
-    phase = tf.pad(phase, paddings, 'SYMMETRIC')
-    # phase = tf.pad(phase, paddings, 'REFLECT')
-    absorption = tf.pad(absorption, paddings, 'SYMMETRIC')
-    # phase = phase
-    # absorption = absorption
-    abfs = tf.complex(-absorption, phase)
-    abfs = tf.exp(abfs)
-    ifp = tf.abs(tf.signal.ifft2d(ff * tf.signal.fft2d(abfs))) ** 2
-    ifp = tf.reshape(ifp, [ifp.shape[0], ifp.shape[1], 1])
-    ifp = tf.image.central_crop(ifp, 0.5)
-    ifp = tf.image.per_image_standardization(ifp)
-    ifp = tf.reshape(ifp, [1, ifp.shape[0], ifp.shape[1], 1])
-    # ifp = tfnor_phase(ifp)
-    return ifp
+# def phase_fresnel(phase, absorption, ff, px):
+#     paddings = tf.constant([[px // 2, px // 2], [px // 2, px // 2]])
+#     # padding1 = tf.constant([[px // 2, px // 2], [0, 0]])
+#     # padding2 = tf.constant([[0, 0], [px // 2, px // 2]])
+#     pvalue = tf.reduce_mean(phase[:100, :])
+#     # phase = tf.pad(phase, paddings, 'CONSTANT',constant_values=1)
+#     phase = tf.pad(phase, paddings, 'SYMMETRIC')
+#     # phase = tf.pad(phase, paddings, 'REFLECT')
+#     absorption = tf.pad(absorption, paddings, 'SYMMETRIC')
+#     # phase = phase
+#     # absorption = absorption
+#     abfs = tf.complex(-absorption, phase)
+#     abfs = tf.exp(abfs)
+#     ifp = tf.abs(tf.signal.ifft2d(ff * tf.signal.fft2d(abfs))) ** 2
+#     ifp = tf.reshape(ifp, [ifp.shape[0], ifp.shape[1], 1])
+#     ifp = tf.image.central_crop(ifp, 0.5)
+#     ifp = tf.image.per_image_standardization(ifp)
+#     ifp = tf.reshape(ifp, [1, ifp.shape[0], ifp.shape[1], 1])
+#     # ifp = tfnor_phase(ifp)
+#     return ifp
 
 
-def phase_fraunhofer(phase, absorption):
-    wf = tf.complex(absorption, phase)
-    # wf = tf.complex(phase, absorption)
+# def phase_fraunhofer(phase, absorption):
+#     wf = tf.complex(absorption, phase)
+#     # wf = tf.complex(phase, absorption)
 
-    # wf = mask_img(wf)
-    # wf = tf.multiply(ampl, tf.exp(phshift))
-    # wf = tf.manip.roll(wf, [160, 160], [0, 1])
-    ifp = tf.square(tf.abs(tf.signal.fft2d(wf)))
+#     # wf = mask_img(wf)
+#     # wf = tf.multiply(ampl, tf.exp(phshift))
+#     # wf = tf.manip.roll(wf, [160, 160], [0, 1])
+#     ifp = tf.square(tf.abs(tf.signal.fft2d(wf)))
  
     
-    # # adding log to the fft
-    # ifp = tf.math.log(ifp+8000)
-    ifp = tf.math.log(ifp+10000)
-    # ifp = tf.math.log(tf.abs(tf.signal.fft2d(wf))+1)
-    # ifp = tf.math.log(tf.square(tf.abs(tf.signal.fft2d(wf)))+1)
-    ifp = tf.signal.fftshift(ifp)
+#     # # adding log to the fft
+#     # ifp = tf.math.log(ifp+8000)
+#     ifp = tf.math.log(ifp+10000)
+#     # ifp = tf.math.log(tf.abs(tf.signal.fft2d(wf))+1)
+#     # ifp = tf.math.log(tf.square(tf.abs(tf.signal.fft2d(wf)))+1)
+#     ifp = tf.signal.fftshift(ifp)
   
-    # ifp = tf.roll(ifp, [256, 256], [0, 1])
-    ifp = tf.reshape(ifp, [1, ifp.shape[0], ifp.shape[1], 1])
-    ifp = tf.image.per_image_standardization(ifp)
-    ifp = tfnor_diff(ifp)
-    return ifp
+#     # ifp = tf.roll(ifp, [256, 256], [0, 1])
+#     ifp = tf.reshape(ifp, [1, ifp.shape[0], ifp.shape[1], 1])
+#     ifp = tf.image.per_image_standardization(ifp)
+#     ifp = tfnor_diff(ifp)
+#     return ifp
 
 
 class GANrec:
@@ -308,7 +314,7 @@ class GANrec:
 
 class GANtomo:
     def __init__(self, prj_input, angle, **kwargs):
-        tomo_args = _get_GANtomo_kwargs()
+        tomo_args = config['GANtomo']
         tomo_args.update(**kwargs)
         super(GANtomo, self).__init__()
         self.prj_input = prj_input
@@ -354,17 +360,23 @@ class GANtomo:
         checkpoint = tf.train.Checkpoint(generator_optimizer=self.generator_optimizer,
                                          discriminator_optimizer=self.discriminator_optimizer,
                                          generator=self.generator,
+    
                                          discriminator=self.discriminator)
+        
+    @tf.function    
+    def tfnor_tomo(img):
+        img = tf.image.per_image_standardization(img)
+        img = img / tf.reduce_max(img)
+        img = img - tf.reduce_min(img)
+        return img
 
     @tf.function
-    def recon_step(self, prj, ang):
-        # noise = tf.random.normal([1, 181, 366, 1])
-        # noise = tf.cast(noise, dtype=tf.float32)
+    def recon_step(self, prj, ang):      
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            # tf.print(tf.reduce_min(sino), tf.reduce_max(sino))
             recon = self.generator(prj)
             recon = tfnor_tomo(recon)
-            prj_rec = tomo_radon(recon, ang)
+            tomo_radon_obj = TomoRadon(recon, ang)
+            prj_rec = tomo_radon_obj.radon()
             prj_rec = tfnor_tomo(prj_rec)
             real_output = self.discriminator(prj, training=True)
             fake_output = self.discriminator(prj_rec, training=True)
@@ -383,45 +395,11 @@ class GANtomo:
                 'g_loss': g_loss,
                 'd_loss': d_loss}
 
-    def recon_step_filter(self, prj, ang):
-        with tf.GradientTape() as filter_tape, tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            # tf.print(tf.reduce_min(sino), tf.reduce_max(sino))
-            prj_filter = self.filter(prj)
-            prj_filter = tfnor_data(prj_filter)
-            recon = self.generator(prj_filter)
-            recon = tfnor_data(recon)
-            prj_rec = tomo_radon(recon, ang)
-            prj_rec = tfnor_data(prj_rec)
-            real_output = self.discriminator(prj, training=True)
-            filter_output = self.discriminator(prj_filter, training=True)
-            fake_output = self.discriminator(prj_rec, training=True)
-            f_loss = filer_loss(filter_output, prj, prj_filter)
-            g_loss = generator_loss(fake_output, prj_filter, prj_rec, self.l1_ratio)
-            d_loss = discriminator_loss(real_output, fake_output)
-        gradients_of_filter = filter_tape.gradient(f_loss,
-                                                   self.filter.trainable_variables)
-        gradients_of_generator = gen_tape.gradient(g_loss,
-                                                   self.generator.trainable_variables)
-        gradients_of_discriminator = disc_tape.gradient(d_loss,
-                                                        self.discriminator.trainable_variables)
-        self.filter_optimizer.apply_gradients(zip(gradients_of_filter,
-                                                  self.filter.trainable_variables))
-        self.generator_optimizer.apply_gradients(zip(gradients_of_generator,
-                                                     self.generator.trainable_variables))
-        self.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator,
-                                                         self.discriminator.trainable_variables))
-        return {'recon': recon,
-                'prj_filter': prj_filter,
-                'prj_rec': prj_rec,
-                'g_loss': g_loss,
-                'd_loss': d_loss}
-
     @property
     def recon(self):
         nang, px = self.prj_input.shape
         prj = np.reshape(self.prj_input, (1, nang, px, 1))
         prj = tf.cast(prj, dtype=tf.float32)
-        # prj = tfnor_data(prj)
         ang = tf.cast(self.angle, dtype=tf.float32)
         self.make_model()
         if self.init_wpath:
