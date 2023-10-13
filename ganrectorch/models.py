1f67112c197709ea7a10ba4d36fef84b816294da
dawitstranger@gmail.com <dawitk27@gmail.com>
2023-10-13 20:48:52 +0200

    updated model with a different gen_loss function which includes contrast transformation and others. The gpu usaga has also been optimized where there is no memory leakage.

--- ganrectorch/models.py
+++ ganrectorch/models.py
@@ -272,6 +272,30 @@ class make_dis(nn.Module):
     def forward_discriminator(self, x):
         self.likelihood = self.discriminator_model(x)
         return self.likelihood
+    
+class transform_images(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.transformed_images = to_device(transformed_images, self.device)
+        self.reshaped = to_device(transform(self.transformed_images, 'reshape') , self.device)
+        self.normalized = to_device(transform(self.transformed_images, 'normalize'), self.device)
+        self.norm = to_device(transform(self.transformed_images, 'norm'), self.device)
+        self.contrast = to_device(transform(self.transformed_images, 'contrast', self.transform_factor), self.device)
+        self.brightness = to_device(transform(self.transformed_images, 'brightness', self.transform_factor), self.device)
+
+        #a function that returns the transformed images
+    def transform(self, x):
+        if 'transform_type' in self.kwargs.keys():
+            transform_type = self.kwargs['transform_type']
+        else:
+            transform_type = 'normalize'
+
+        if 'transform_factor' in self.kwargs.keys():
+            transform_factor = self.kwargs['transform_factor']
+        else:
+            transform_factor = 0.5
+        return transform(x, transform_type, transform_factor)
+
 
 class make_ganrec_model(nn.Module):
     def __init__(self, shape_x, shape_y, conv_num, conv_size, dropout, output_num, fresnel_factor, transformed_images=None, device=None, **kwargs):
@@ -367,6 +391,14 @@ class make_ganrec_model(nn.Module):
             *discriminator_stack
         ), self.device)
 
+        self.reshaped = to_device(transform(self.transformed_images, 'reshape') , self.device)
+        self.normalized = to_device(transform(self.transformed_images, 'normalize'), self.device)
+        self.norm = to_device(transform(self.transformed_images, 'norm'), self.device)
+        self.contrast = to_device(transform(self.transformed_images, 'contrast', self.transform_factor), self.device)
+        self.contrast_normalize = to_device(transform(self.transformed_images, 'constrast_normalize', self.transform_factor), self.device)
+        self.brightness = to_device(transform(self.transformed_images, 'brightness', self.transform_factor), self.device)
+        self.brightness_normalize = to_device(transform(self.transformed_images, 'brightness_normalize', self.transform_factor), self.device)
+        
     def init_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
@@ -388,22 +420,40 @@ class make_ganrec_model(nn.Module):
         self.likelihood = self.discriminator_model(x)
         return self.likelihood
     
+    def transform(self, x):
+        if 'transform_type' in self.kwargs.keys():
+            transform_type = self.kwargs['transform_type']
+        else:
+            transform_type = 'normalize'
+
+        if 'transform_factor' in self.kwargs.keys():
+            transform_factor = self.kwargs['transform_factor']
+        else:
+            transform_factor = 0.5
+        return transform(x, transform_type, transform_factor)
+    
     def propagator(self):
-        phase = (self.pred[:,0,:,:])
-        attenuation =  (1 - (self.pred[:,1,:,:]))*self.abs_ratio
-        propagated_intensity = torchnor_phase(forward_propagate(shape_x = self.shape_x, shape_y = self.shape_y, pad = self.pad, energy_kev = self.energy_kev, detector_pixel_size = self.detector_pixel_size, distance_sample_detector = self.distance_sample_detector, phase_image = phase, attenuation_image = attenuation, fresnel_factor  = self.fresnel_factor, wavefield = None, distance =  self.distance_sample_detector, mode = self.mode, value = self.value))
+        phase = transform(self.pred[:,0,:,:])
+        attenuation =  (1 - transform(self.pred[:,1,:,:]))*self.abs_ratio
+        propagated_intensity = transform(forward_propagate(shape_x = self.shape_x, shape_y = self.shape_y, pad = self.pad, energy_kev = self.energy_kev, detector_pixel_size = self.detector_pixel_size, distance_sample_detector = self.distance_sample_detector, phase_image = phase, attenuation_image = attenuation, fresnel_factor  = self.fresnel_factor, wavefield = None, distance =  self.distance_sample_detector, mode = self.mode, value = self.value))
         return propagated_intensity, phase, attenuation
     
     def forward(self, x):
-        self.pred = self.generator_model(x)
+        self.pred = self.generator(x)
         propagated_intensity, phase, attenuation = self.propagator()
-        self.fake_output = self.discriminator_model(propagated_intensity)
-        self.real_output = self.discriminator_model(self.transformed_images)
+        self.fake_output = self.discriminator(propagated_intensity)
+        self.real_output = self.discriminator(self.transformed_images)
         return self.fake_output, self.real_output, propagated_intensity, phase, attenuation
     
-    def generator_loss(self, fake_output, img_output, pred, l1_ratio = 10):
+    def generator_loss(self, fake_output, image_output, propagated_intensity, l1_ratio = 10):
         return torch.mean(torch.nn.BCEWithLogitsLoss()(fake_output, torch.ones_like(fake_output))) + \
-                  l1_ratio * torch.mean(torch.abs(img_output - pred))
+                l1_ratio * torch.mean(torch.abs(self.reshaped - transform(propagated_intensity, 'reshape', 0.5))) + \
+                l1_ratio * torch.mean(torch.abs(self.contrast_normalize - transform(propagated_intensity, 'contrast_normalize', self.transform_factor))) + \
+                    1/2*l1_ratio * torch.mean(torch.abs(self.brightness_normalize - transform(propagated_intensity, 'brightness_normalize', self.transform_factor))) + \
+                    1/2*l1_ratio * torch.mean(torch.abs(self.norm - transform(propagated_intensity, 'norm', 0.5)))
+                    # l1_ratio * torch.mean(torch.abs(self.normalized - transform(propagated_intensity, 'normalize', 0.5))) +\
+                
+    
     
     def discriminator_loss(self, real_output, fake_output):
         real_loss = torch.mean(torch.nn.BCEWithLogitsLoss()(real_output, torch.ones_like(real_output)))
@@ -415,7 +465,7 @@ class make_ganrec_model(nn.Module):
         self.generator_optimizer.zero_grad()
         self.discriminator_optimizer.zero_grad()
         self.fake_output, self.real_output, self.propagated_intensity, self.phase, self.attenuation = self.forward(x)
-        self.gen_loss = self.generator_loss(self.fake_output, self.transformed_images, self.pred)
+        self.gen_loss = self.generator_loss(self.fake_output, self.transformed_images, self.propagated_intensity)
         self.dis_loss = self.discriminator_loss(self.real_output, self.fake_output)
         self.gen_loss.backward(retain_graph=True)
         self.dis_loss.backward()
@@ -423,13 +473,17 @@ class make_ganrec_model(nn.Module):
         self.discriminator_optimizer.step()
         return self.gen_loss, self.dis_loss, self.propagated_intensity, self.phase, self.attenuation
     
-    def train(self, iter_num = 100, save_model = False, save_model_path = None):
+    def train(self, iter_num = None, save_model = None, save_model_path = None):
         if self.device is None:
             self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
             self.transformed_images = to_device(self.transformed_images, self.device)
             self.fresnel_factor = to_device(self.fresnel_factor, self.device)
             self.generator.to(self.device)
             self.discriminator.to(self.device)
+    
+        iter_num = self.iter_num if iter_num is None else iter_num
+        save_model = self.save_model if save_model is None else save_model
+        save_model_path = self.save_model_path if save_model_path is None else save_model_path
 
         self.make_model()
         self.gen_loss_list = []
@@ -438,14 +492,14 @@ class make_ganrec_model(nn.Module):
         self.phase_list = []
         self.attenuation_list = []
         for i in range(iter_num):
-            self.gen_loss, self.dis_loss, self.propagated_intensity, self.phase, self.attenuation = self.train_step(self.transformed_images)
-            self.gen_loss_list.append(self.gen_loss)
-            self.dis_loss_list.append(self.dis_loss)
-            self.propagated_intensity_list.append(self.propagated_intensity)
-            self.phase_list.append(self.phase)
-            self.attenuation_list.append(self.attenuation)
+            self.g_loss, self.d_loss, self.propagated_intensity, self.phase, self.attenuation = self.train_step(self.transformed_images)
+            self.gen_loss_list.append(tensor_to_np(self.gen_loss))
+            self.dis_loss_list.append(tensor_to_np(self.dis_loss))
+            self.propagated_intensity_list.append(tensor_to_np(self.propagated_intensity))
+            self.phase_list.append(tensor_to_np(self.phase))
+            self.attenuation_list.append(tensor_to_np(self.attenuation))
             if i % 100 == 0:
-                print('gen_loss: ', self.gen_loss, 'dis_loss: ', self.dis_loss)
+                print('gen_loss: ', self.g_loss, 'dis_loss: ', self.d_loss)
         if save_model:
             torch.save(self.generator.state_dict(), save_model_path)
         return self.gen_loss_list, self.dis_loss_list, self.propagated_intensity_list, self.phase_list, self.attenuation_list
@@ -463,35 +517,39 @@ class make_ganrec_model(nn.Module):
         self.attenuation_list = []
         for i in range(iter_num):
             self.gen_loss, self.dis_loss, self.propagated_intensity, self.phase, self.attenuation = self.train_step(testloader)
-            self.gen_loss_list.append(self.gen_loss)
-            self.dis_loss_list.append(self.dis_loss)
-            self.propagated_intensity_list.append(self.propagated_intensity)
-            self.phase_list.append(self.phase)
-            self.attenuation_list.append(self.attenuation)
+            self.gen_loss_list.append(tensor_to_np(self.gen_loss))
+            self.dis_loss_list.append(tensor_to_np(self.dis_loss))
+            self.propagated_intensity_list.append(tensor_to_np(self.propagated_intensity))
+            self.phase_list.append(tensor_to_np(self.phase))
+            self.attenuation_list.append(tensor_to_np(self.attenuation))
             if i % 10 == 0:
                 print('gen_loss: ', self.gen_loss, 'dis_loss: ', self.dis_loss)
         if save_model:
             torch.save(self.generator.state_dict(), save_model_path)
         return self.gen_loss_list, self.dis_loss_list, self.propagated_intensity_list, self.phase_list, self.attenuation_list
 
-    def live_plot(self):
+    def live_plot(self, iter_num = None, rate = 1):
         import matplotlib.pyplot as plt
         from IPython.display import clear_output
-        for i in range(self.iter_num):
-            clear_output(wait=True)
-            plt.figure(figsize=(20,10))
-            plt.subplot(1,3,3)
-            plt.plot(tensor_to_np(self.gen_loss_list), label='gen_loss')
-            plt.plot(tensor_to_np(self.dis_loss_list), label='dis_loss')
-            plt.title('iteration: '+str(i))
-            plt.legend()
-            plt.subplot(1,3,1)
-            plt.title('propagated_intensity')
-            plt.imshow(tensor_to_np(self.propagated_intensity_list[i]), cmap='gray')
-            plt.colorbar()
-            plt.subplot(1,3,2)
-            plt.title('phase')
-            plt.imshow(tensor_to_np(self.phase_list[i]), cmap='coolwarm')
-            plt.colorbar()
-            plt.gca()
+        if iter_num is None:
+            iter_num = self.iter_num
+        for i in range(iter_num):
+            clear_output(wait=True) 
+            if i % rate == 0:       
+                plt.figure(figsize=(20,10))                            
+                plt.subplot(1,3,3)
+                plt.subplot(1,3,3)
+                plt.plot(self.gen_loss_list, label='gen_loss')
+                plt.plot(self.dis_loss_list, label='dis_loss')
+                plt.title('iteration: '+str(i))
+                plt.legend()
+                plt.subplot(1,3,1)
+                plt.title('propagated_intensity')
+                plt.imshow(self.propagated_intensity_list[i], cmap='gray')
+                plt.colorbar()
+                plt.subplot(1,3,2)
+                plt.title('phase')
+                plt.imshow(self.phase_list[i], cmap='coolwarm')
+                plt.colorbar()
+                plt.gca()
             plt.show()
