0f6f55147e0509ee0e69b81d35e1200ea3d41a34
“XYangXRay” <xiaogang.yang@outlook.com>
2024-05-07 21:31:09 -0400

    add GANtensor to the ganrec2.py

--- ganrec/ganrec2.py
+++ ganrec/ganrec2.py
@@ -4,7 +4,7 @@ import json
 import tensorflow as tf
 import tensorflow_addons as tfa
 
-from ganrec.propagators import TomoRadon, PhaseFresnel, PhaseFraunhofer
+from ganrec.propagators import TomoRadon, TensorRadon, PhaseFresnel, PhaseFraunhofer
 from ganrec.models import make_generator, make_generator_diff, make_generator_fno, make_discriminator, make_filter, diffusion_model
 from ganrec.utils import RECONmonitor, ffactor
 
@@ -489,6 +489,132 @@ class GANtomo:
         recon_monitor.close_plot()
         return recon[epoch]
         # return avg_results(recon, gen_loss)
+
+
+
+class GANtensor:
+    def __init__(self, prj_input, angle, psi, **kwargs):
+        tomo_args = config['GANtensor']
+        tomo_args.update(**kwargs)
+        super(GANtensor, self).__init__()
+        self.prj_input = prj_input
+        self.angle = angle
+        self.psi = psi
+        self.iter_num = tomo_args['iter_num']
+        self.conv_num = tomo_args['conv_num']
+        self.conv_size = tomo_args['conv_size']
+        self.dropout = tomo_args['dropout']
+        self.l1_ratio = tomo_args['l1_ratio']
+        self.g_learning_rate = tomo_args['g_learning_rate']
+        self.d_learning_rate = tomo_args['d_learning_rate']
+        self.save_wpath = tomo_args['save_wpath']
+        self.init_wpath = tomo_args['init_wpath']
+        self.init_model = tomo_args['init_model']
+        self.recon_monitor = tomo_args['recon_monitor']
+        self.filter = None
+        self.generator = None
+        self.discriminator = None
+        self.filter_optimizer = None
+        self.generator_optimizer = None
+        self.discriminator_optimizer = None
+    def make_model(self):
+        self.filter = make_filter(self.prj_input.shape[0],
+                                  self.prj_input.shape[1])
+  
+        self.generator = make_generator(self.prj_input.shape[0],
+                                        self.prj_input.shape[1],
+                                        self.conv_num,
+                                        self.conv_size,
+                                        self.dropout,
+                                        6)        
+        self.discriminator = make_discriminator(self.prj_input.shape[0],
+                                                self.prj_input.shape[1])
+        self.generator_optimizer = tf.keras.optimizers.Adam(self.g_learning_rate)
+        self.discriminator_optimizer = tf.keras.optimizers.Adam(self.d_learning_rate)
+        self.generator.compile()
+        self.discriminator.compile()
+        
+    @tf.function    
+    def tfnor_tomo(self, img):
+        img = tf.image.per_image_standardization(img)
+        img = (img - tf.reduce_min(img)) / (tf.reduce_max(img) - tf.reduce_min(img))
+        return img
+    @tf.function
+    def recon_step(self, prj, ang, psi):      
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            recon = self.generator(prj)
+            recon = self.tfnor_tomo(recon)
+            tomo_radon_obj = TensorRadon(recon, ang, psi)
+            prj_rec = tomo_radon_obj.compute()
+            prj_rec = self.tfnor_tomo(prj_rec)
+            real_output = self.discriminator(prj, training=True)
+            fake_output = self.discriminator(prj_rec, training=True)
+            g_loss = generator_loss(fake_output, prj, prj_rec, self.l1_ratio)
+            d_loss = discriminator_loss(real_output, fake_output)
+        gradients_of_generator = gen_tape.gradient(g_loss,
+                                                   self.generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(d_loss,
+                                                        self.discriminator.trainable_variables)
+        self.generator_optimizer.apply_gradients(zip(gradients_of_generator,
+                                                     self.generator.trainable_variables))
+        self.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator,
+                                                         self.discriminator.trainable_variables))
+        return {'recon': recon,
+                'prj_rec': prj_rec,
+                'g_loss': g_loss,
+                'd_loss': d_loss}
+    @property
+    def recon(self):
+        nang, px = self.prj_input.shape
+        prj = np.reshape(self.prj_input, (1, nang, px, 1)) 
+      
+        prj = tf.cast(prj, dtype=tf.float32)
+        prj = self.tfnor_tomo(prj)
+        ang = tf.cast(self.angle, dtype=tf.float32)
+        psi = self.psi
+        self.make_model()
+        if self.init_wpath:
+            self.generator.load_weights(self.init_wpath+'generator.h5')
+            print('generator is initilized')
+            self.discriminator.load_weights(self.init_wpath+'discriminator.h5')
+        recon = np.zeros((self.iter_num, px, px, 1))
+        gen_loss = np.zeros((self.iter_num))
+
+        ###########################################################################
+        # Reconstruction process monitor
+        if self.recon_monitor:
+            plot_x, plot_loss = [], []
+            recon_monitor = RECONmonitor('tomo')
+            recon_monitor.initial_plot(self.prj_input)
+        ###########################################################################
+        for epoch in range(self.iter_num):
+            
+            step_result = self.recon_step(prj, ang, psi)
+     
+            recon[epoch, :, :, :] = step_result['recon']
+            gen_loss[epoch] = step_result['g_loss']
+         
+            ###########################################################################
+
+            plot_x.append(epoch)
+            plot_loss = gen_loss[:epoch + 1]
+            if (epoch + 1) % 100 == 0:
+                if recon_monitor:
+                    prj_rec = np.reshape(step_result['prj_rec'], (nang, px))
+                    prj_diff = np.abs(prj_rec - self.prj_input.reshape((nang, px)))
+                    rec_plt = np.reshape(recon[epoch], (px, px))
+                    recon_monitor.update_plot(epoch, prj_diff, rec_plt, plot_x, plot_loss)
+                print('Iteration {}: G_loss is {} and D_loss is {}'.format(epoch + 1,
+                                                                           gen_loss[epoch],
+                                                                           step_result['d_loss'].numpy()))
+            # plt.close()
+        if self.save_wpath != None:
+            self.generator.save(self.save_wpath+'generator.h5')
+            self.discriminator.save(self.save_wpath+'discriminator.h5')
+        recon_monitor.close_plot()
+        return recon[epoch]
+      
+
 
 class GANtomo3D:
     def __init__(self, prj_input, angle, **kwargs):
--- ganrec/propagators.py
+++ ganrec/propagators.py
@@ -17,7 +17,37 @@ class TomoRadon:
         sino = tf.transpose(sino, [2, 0, 1])
         sino = tf.reshape(sino, [sino.shape[0], sino.shape[1], sino.shape[2], 1])
         return sino
-    
+
+
+class TensorRadon:
+
+    def __init__(self, strain_tensor, ang, psi):
+        self.rec = strain_tensor
+        self.ang = ang
+        self.psi = psi
+
+    def compute(self):
+        detector_rows = self.strain_tensor.shape[1]
+        detector_columns = self.strain_tensor.shape[2]
+        vol_mask = tf.zeros((detector_rows, detector_columns, detector_columns))
+        vol_mask = tf.reduce_sum(tf.abs(strain_tensor), axis=0) > 0.0
+        vol_mask = tf.reshape(vol_mask, (-1, detector_columns, detector_columns, 1))
+        vol_mask = tf.cast(vol_mask, dtype=tf.float32)
+        strain_tensor = tf.cast(self.strain_tensor, dtype=tf.float32)
+        angles = tf.cast(self.ang, dtype=tf.float32)
+        thickness = TomoRadon(vol_mask, angles).compute()
+        thickness = tf.squeeze(thickness)
+        strain_tensor = tf.transpose(strain_tensor, [0, 2, 3, 1])
+        proj_strain_comp = TomoRadon(strain_tensor, angles).compute()
+        proj_strain_comp = tf.squeeze(proj_strain_comp)
+        proj_strain_ws = tf.multiply(proj_strain_comp[0], tf.expand_dims(tf.pow(tf.cos(angles), 2) * tf.pow(tf.sin(self.psi), 2), 1)) + \
+            tf.multiply(proj_strain_comp[1], tf.expand_dims(tf.pow(tf.sin(angles), 2) * tf.pow(tf.sin(self.psi), 2), 1)) + \
+                tf.multiply(proj_strain_comp[2], tf.pow(tf.cos(self.psi), 2), 1) + \
+                    tf.multiply(proj_strain_comp[3], tf.expand_dims(tf.sin(2 * angles) * tf.pow(tf.sin(self.psi), 2), 1)) + \
+                        tf.multiply(proj_strain_comp[4], tf.expand_dims(tf.sin(angles) * tf.sin(2 * self.psi), 1)) + \
+                            tf.multiply(proj_strain_comp[5], tf.expand_dims(tf.cos(angles) * tf.sin(2 * self.psi), 1))
+        strain_sino = tf.math.divide_no_nan(proj_strain_ws, thickness)
+        return strain_sino    
     
 class PhaseFresnel:
     
