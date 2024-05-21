5862f6623e466324dd027a75502d722a0568dee3
“XYangXRay” <xiaogang.yang@outlook.com>
2024-05-20 20:36:13 -0400

    clean up the ganrectorch folder

--- ganrectorch/utils.py
+++ ganrectorch/utils.py
@@ -6,6 +6,11 @@ import tifffile
 import matplotlib.pyplot as plt
 from IPython.display import display, clear_output
 
+from numpy import pi 
+import skimage.io as io
+from skimage.transform import resize
+import quantities as pq
+
 def nor_tomo(img):
     mean_tmp = np.mean(img)
     std_tmp = np.std(img)
@@ -62,66 +67,6 @@ def ffactor(px, energy, z, pv):
     eta = eta.astype('float32')
     h = np.exp(- 1j * frequ_prefactor * (xi ** 2 + eta ** 2) / 2)
     return h
-
-
-
-
-
-# class RECONmonitor:
-#     def __init__(self):
-#         self.recon_target = recon_target
-#         self.img_input = tensor_to_np(img_input)
-#         self.fig, self.axs = plt.subplots(2, 2, figsize=(16, 8))
-
-
-#     # def __init__(self, recon_target):
-#     #     self.fig, self.axs = plt.subplots(2, 2, figsize=(16, 8))
-#     #     self.recon_target = recon_target
-#     #     if self.recon_target == 'tomo':
-#     #         self.plot_txt = 'Sinogram'
-#     #     elif self.recon_target == 'phase':
-#     #         self.plot_txt = 'Intensity'
-      
-
-#     def initial_plot(self, img_input):
-#         if self.recon_target == 'tomo':
-#             self.plot_txt = 'Sinogram'
-#             self.dummy_img1 = np.zeros((self.img_input.shape[1], self.img_input.shape[1]))
-#             self.dummy_img2 = np.zeros_like(self.img_input)
-#         elif self.recon_target == 'phase':
-#             self.plot_txt = 'Intensity'
-#         _, px = img_input.shape
-#         self.im0 = self.axs[0, 0].imshow(img_input, cmap='gray')
-#         self.axs[0, 0].set_title(self.plot_txt)
-#         self.fig.colorbar(self.im0, ax=self.axs[0, 0])
-#         self.axs[0, 0].set_aspect('equal','box')
-#         self.im1 = self.axs[1, 0].imshow(img_input, cmap='jet')
-#         self.tx1 = self.axs[1, 0].set_title('Difference of ' + self.plot_txt + ' for iteration 0')
-#         self.fig.colorbar(self.im1, ax=self.axs[1, 0])
-#         self.axs[1, 0].set_aspect('equal')
-#         self.im2 = self.axs[0, 1].imshow(np.zeros((px, px)), cmap='gray')
-#         self.fig.colorbar(self.im2, ax=self.axs[0, 1])
-#         self.axs[0, 1].set_title('Reconstruction')
-#         self.im3, = self.axs[1, 1].plot([], [], 'r-')
-#         self.axs[1, 1].set_title('Generator loss')
-#         plt.tight_layout()
-
-#     def update_plot(self, epoch, img_diff, img_rec, plot_x, plot_loss):
-#         self.tx1.set_text('Difference of ' + self.plot_txt + ' for iteration {0}'.format(epoch))
-#         vmax = np.max(img_diff)
-#         vmin = np.min(img_diff)
-#         self.im1.set_data(img_diff)
-#         self.im1.set_clim(vmin, vmax)
-#         self.im2.set_data(img_rec)
-#         vmax = np.max(img_rec)
-#         vmin = np.min(img_rec)
-#         self.im2.set_clim(vmin, vmax)
-#         self.axs[1, 1].plot(plot_x, plot_loss, 'r-')
-#         # plt.tight_layout()
-#         plt.pause(0.1)
-#     def close_plot(self):
-#         plt.close()
-
 
 
 class RECONmonitor:
@@ -227,14 +172,6 @@ def save_tiff(image, filename):
 
 
 
-import numpy as np
-from numpy.fft import fftfreq
-from numpy import pi 
-import matplotlib.pyplot as plt
-import os 
-import skimage.io as io
-from skimage.transform import resize
-import quantities as pq
 
 def nor_phase(img):
     mean_tmp = np.mean(img)
