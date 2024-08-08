a62936120c2447076fc722abc2751e2e3f5a6923
“XYangXRay” <xiaogang.yang@outlook.com>
2024-08-08 15:46:04 -0400

    correct the phase value issue

--- simluations/fresnel.py
+++ simluations/fresnel.py
@@ -74,14 +74,14 @@ def propagate(img_f, img_a, energy, z, pv, abs_ratio):
     if is_3d:
         for i in range(img_f.shape[0]):
             phase = img_f[i]
-            phase = padding(phase, padded_size, pvalue=phase.min())
+            phase = padding(phase, padded_size, pvalue=phase.max())
             absorption = img_a[i]
             absorption = padding(absorption, padded_size, pvalue=absorption.min())          
             ifp_test = fresnel_propagation(phase, absorption, h, abs_ratio)
             ifp_test = unpadding(ifp_test, cropped_size)
             ifp_tests.append(ifp_test)
     else:
-        phase = padding(img_f, padded_size, pvalue=img_f.min())
+        phase = padding(img_f, padded_size, pvalue=img_f.max())
         absorption = padding(img_a, padded_size, pvalue=img_a.min())  
         ifp_test = fresnel_propagation(phase, absorption, h, abs_ratio)
         ifp_test = unpadding(ifp_test, cropped_size)
